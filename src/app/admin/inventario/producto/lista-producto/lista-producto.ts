849ea76d3bed3cdadc2ddc2c0660a05f9ccc035e
Cristian <cchura.cpc@gmail.com>
2026-07-23 22:34:27 -0400

    paginacion productos

--- src/app/admin/inventario/producto/lista-producto/lista-producto.ts
+++ src/app/admin/inventario/producto/lista-producto/lista-producto.ts
@@ -1,27 +1,48 @@
-import { Component, inject, OnInit, signal } from '@angular/core';
+import { AfterViewInit, Component, inject, OnDestroy, OnInit, signal, ViewChild } from '@angular/core';
 import { ProductoService } from '../../../../core/services/producto.service';
+import { MatCardModule } from "@angular/material/card";
+import { MatFormField, MatFormFieldModule } from "@angular/material/form-field";
+import { MatInput, MatInputModule } from "@angular/material/input";
+import { MatTable, MatTableModule } from "@angular/material/table";
+import { MatPaginator, MatPaginatorModule } from '@angular/material/paginator';
+import { tap } from 'rxjs';
 
 @Component({
   selector: 'app-lista-producto',
-  imports: [],
+  imports: [MatCardModule, MatFormFieldModule, MatInputModule, MatTableModule, MatPaginatorModule],
   templateUrl: './lista-producto.html',
   styleUrl: './lista-producto.scss',
 })
-export class ListaProducto implements OnInit{
+export class ListaProducto implements AfterViewInit{
+ 
+  displayedColumns: string[] = ['id', 'nombre', 'precio'];
 
   productoService = inject(ProductoService);
 
   lista_productos = signal<any[]>([]);
+  total = signal<number>(0);
 
-  ngOnInit(): void{
+  @ViewChild(MatPaginator) paginator!: MatPaginator;
+
+ 
+
+  ngAfterViewInit(): void {
+    
     this.funListaProductos()
+
+    this.paginator.page.pipe(
+      tap(() => this.funListaProductos())
+    ).subscribe()
+
+   
   }
 
   funListaProductos(){
-    this.productoService.listar(1, 5).subscribe({
+    this.productoService.listar(this.paginator?.pageIndex + 1, this.paginator?.pageSize).subscribe({
       next: (res: any) => {
           let { data, total } = res;
           this.lista_productos.set(data);
+          this.total.set(total);
       }
     })
   }
